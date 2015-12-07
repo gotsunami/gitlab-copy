aeb0fc4680035dadf6851514be821709b3b05174
matm <matm@outofpluto.com>
2015-12-07 22:20:56 +0100

    Show source project stats

--- src/glm/main.go
+++ src/glm/main.go
@@ -6,12 +6,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/xanzy/go-gitlab"
 )
 
 const (
-	resultsPerPage = 50
+	resultsPerPage = 100
 )
 
 // GitLab server endpoints
@@ -63,6 +64,130 @@ func (m *migration) destProject(name string) (*gitlab.Project, error) {
 	return m.project(m.endpoint.to, name)
 }
 
+func map2human(m map[string]int) string {
+	keys := make([]string, len(m))
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+	return strings.Join(keys, ", ")
+}
+
+type projectStats struct {
+	project                               *gitlab.Project
+	nbIssues, nbClosed, nbOpened, nbNotes int
+	milestones, labels                    map[string]int
+}
+
+func newProjectStats(prj *gitlab.Project) *projectStats {
+	p := new(projectStats)
+	p.project = prj
+	p.milestones = make(map[string]int)
+	p.labels = make(map[string]int)
+	return p
+}
+
+func (p *projectStats) String() string {
+	return fmt.Sprintf("%d issues (%d opened, %d closed)", p.nbIssues, p.nbOpened, p.nbClosed)
+}
+
+func (p *projectStats) pagination(client *gitlab.Client, f func(*gitlab.Client, *gitlab.ListOptions) (bool, error)) error {
+	if client == nil {
+		return errors.New("nil client")
+	}
+
+	curPage := 1
+	opts := &gitlab.ListOptions{PerPage: resultsPerPage, Page: curPage}
+
+	for {
+		stop, err := f(client, opts)
+		if err != nil {
+			return err
+		}
+		if stop {
+			break
+		}
+		curPage++
+		opts.Page = curPage
+	}
+	return nil
+}
+
+func (p *projectStats) computeStats(client *gitlab.Client) error {
+	if client == nil {
+		return errors.New("nil client")
+	}
+
+	action := func(c *gitlab.Client, lo *gitlab.ListOptions) (bool, error) {
+		opts := &gitlab.ListProjectIssuesOptions{ListOptions: gitlab.ListOptions{PerPage: lo.PerPage, Page: lo.Page}}
+		issues, _, err := client.Issues.ListProjectIssues(*p.project.ID, opts)
+		if err != nil {
+			return false, err
+		}
+		if len(issues) > 0 {
+			p.nbIssues += len(issues)
+			for _, issue := range issues {
+				switch issue.State {
+				case "opened":
+					p.nbOpened++
+				case "closed":
+					p.nbClosed++
+				}
+				if issue.Milestone.Title != "" {
+					p.milestones[issue.Milestone.Title]++
+				}
+				if len(issue.Labels) > 0 {
+					for _, label := range issue.Labels {
+						p.labels[label]++
+					}
+				}
+			}
+		} else {
+			// Exit
+			return true, nil
+		}
+		return false, nil
+	}
+
+	if err := p.pagination(client, action); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *projectStats) computeIssueNotes(client *gitlab.Client) error {
+	if client == nil {
+		return errors.New("nil client")
+	}
+
+	action := func(c *gitlab.Client, lo *gitlab.ListOptions) (bool, error) {
+		opts := &gitlab.ListProjectIssuesOptions{ListOptions: gitlab.ListOptions{PerPage: lo.PerPage, Page: lo.Page}}
+		issues, _, err := client.Issues.ListProjectIssues(*p.project.ID, opts)
+		if err != nil {
+			return false, err
+		}
+		if len(issues) > 0 {
+			for _, issue := range issues {
+				notes, _, err := client.Notes.ListIssueNotes(*p.project.ID, issue.ID, nil)
+				if err != nil {
+					return false, err
+				}
+				p.nbNotes += len(notes)
+			}
+		} else {
+			// Exit
+			return true, nil
+		}
+		return false, nil
+	}
+
+	if err := p.pagination(client, action); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("missing config file")
@@ -98,43 +223,33 @@ func main() {
 	// Find out how many issues we have
 	fmt.Printf("source: finding issues ... ")
 
-	curPage := 1
-	nbIssues := 0
-	nbClosed, nbOpened := 0, 0
-	milestones := make(map[string]int)
-	opts := &gitlab.ListProjectIssuesOptions{ListOptions: gitlab.ListOptions{PerPage: resultsPerPage, Page: curPage}}
+	pstats := newProjectStats(srcproj)
 
-	for {
-		issues, _, err := m.endpoint.from.Issues.ListProjectIssues(*srcproj.ID, opts)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if len(issues) > 0 {
-			nbIssues += len(issues)
-			curPage++
-			opts.Page = curPage
-			for _, issue := range issues {
-				switch issue.State {
-				case "opened":
-					nbOpened++
-				case "closed":
-					nbClosed++
-				}
-				if issue.Milestone.Title != "" {
-					milestones[issue.Milestone.Title]++
-				}
-			}
-		} else {
-			break
-		}
+	if err := pstats.computeStats(m.endpoint.from); err != nil {
+		log.Fatal(err)
 	}
 	fmt.Println("OK")
-	fmt.Printf("source: %d issues (%d opened, %d closed)\n", nbIssues, nbOpened, nbClosed)
-	keys := make([]string, len(milestones))
-	i := 0
-	for k := range milestones {
-		keys[i] = k
-		i++
+	fmt.Printf("source: %v\n", pstats)
+	if len(pstats.milestones) > 0 {
+		fmt.Printf("source: %d milestone(s): %s\n", len(pstats.milestones), map2human(pstats.milestones))
 	}
-	fmt.Printf("source: %d milestones: %s\n", len(keys), keys)
+	if len(pstats.labels) > 0 {
+		fmt.Printf("source: %d label(s): %s\n", len(pstats.labels), map2human(pstats.labels))
+	}
+
+	fmt.Printf("source: counting notes (comments), can take a while ... ")
+	if err := pstats.computeIssueNotes(m.endpoint.from); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\rsource: %d notes%50s\n", pstats.nbNotes, " ")
+	fmt.Println("--")
+	fmt.Println(`Migration rules are:
+- Create milestone if not existing on target
+- Create label if not existing on target
+- Create issue if not existing on target (by title), either closed of opened on source
+- Creaate note (attached to issue) if not existing on target
+
+Use the --apply option parameter if that looks good to you to start the issues migration.
+`)
+
 }
