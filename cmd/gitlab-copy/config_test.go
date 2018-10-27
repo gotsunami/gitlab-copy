40b2a02016b4aa2148df1f9bd6f24d74f5a4b67a
matm <mathias.monnerville@keeneyetechnologies.com>
2018-10-27 12:09:27 +0200

    Use testify assert library

--- cmd/gitlab-copy/config_test.go
+++ cmd/gitlab-copy/config_test.go
@@ -2,27 +2,33 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestParseConfig(t *testing.T) {
-	if _, err := parseConfig("wrongfile"); err == nil {
-		t.Fatal("expect an error mon missing config file, got nil")
-	}
+	require := require.New(t)
+	_, err := parseConfig("wrongfile")
+	require.NotNil(err)
 }
 
 func TestParseIssues(t *testing.T) {
+	assert := assert.New(t)
+
 	issues := []struct {
+		name       string
 		ranges     []string
 		shouldFail bool
 		expect     []issueRange
 	}{
-		{[]string{"3", "5-10", "Z"}, true, nil},
-		{[]string{"1", "a-10"}, true, nil},
-		{[]string{"1-3-5"}, true, nil},
-		{[]string{"4-8"}, false, []issueRange{{4, 8}}},
-		{[]string{"5-5"}, false, []issueRange{{5, 5}}},
-		{[]string{"2", "5-15", "3"}, false, []issueRange{{2, 2}, {5, 15}, {3, 3}}},
-		{[]string{"4-3"}, true, nil},
+		{"Standalone char in range sequence", []string{"3", "5-10", "Z"}, true, nil},
+		{"Char in range sequence", []string{"1", "a-10"}, true, nil},
+		{"Malformed range", []string{"1-3-5"}, true, nil},
+		{"Valid range, distinct numbers", []string{"4-8"}, false, []issueRange{{4, 8}}},
+		{"Valid range, same number", []string{"5-5"}, false, []issueRange{{5, 5}}},
+		{"Valid ranges, single number", []string{"2", "5-15", "3"}, false, []issueRange{{2, 2}, {5, 15}, {3, 3}}},
+		{"Invalid range bounds", []string{"4-3"}, true, nil},
 	}
 
 	p := new(project)
@@ -30,16 +36,17 @@ func TestParseIssues(t *testing.T) {
 		p.Issues = k.ranges
 		err := p.parseIssues()
 		if err == nil && k.shouldFail {
-			t.Errorf("expects an error for '%s', got nil", p.Issues)
+			t.Errorf("expects an error for %q, got nil", p.Issues)
 		}
 		if err != nil && !k.shouldFail {
-			t.Errorf("expects no error for '%s', got one: %s", p.Issues, err.Error())
+			t.Errorf("expects no error for %q, got one: %s", p.Issues, err.Error())
 		}
 		if k.expect != nil {
 			for j, r := range k.expect {
-				if r.from != p.issues[j].from || r.to != p.issues[j].to {
-					t.Errorf("range mismatch: expects %v, got %v", r, p.issues[j])
-				}
+				t.Run(k.name, func(t *testing.T) {
+					assert.Equal(r.from, p.issues[j].from)
+					assert.Equal(r.to, p.issues[j].to)
+				})
 			}
 		}
 	}
