fdd99be9e546553672a0d5976d0f370ad1d2df13
matm <mathias.monnerville@keeneyetechnologies.com>
2018-11-09 23:37:52 +0100

    UT refactoring

--- migration/issue_test.go
+++ migration/issue_test.go
@@ -18,6 +18,14 @@ func init() {
 	gitlab.DefaultClient = dummyClient
 }
 
+func source(m *Migration) *fakeClient {
+	return m.Endpoint.SrcClient.(*fakeClient)
+}
+
+func dest(m *Migration) *fakeClient {
+	return m.Endpoint.DstClient.(*fakeClient)
+}
+
 func TestMigrate(t *testing.T) {
 	assert := assert.New(t)
 	require := require.New(t)
@@ -26,141 +34,124 @@ func TestMigrate(t *testing.T) {
 	assert.Error(err)
 
 	runs := []struct {
-		name    string           // Sub-test name
-		config  string           // YAML config
-		setup   func(*Migration) // Defines any option before calling Migrate()
-		asserts func(error, *Migration)
+		name    string                     // Sub-test name
+		config  string                     // YAML config
+		setup   func(src, dst *fakeClient) // Defines any option before calling Migrate()
+		asserts func(err error, src, dst *fakeClient)
 	}{
 		{
 			"SourceProject returns an error",
 			cfg1,
-			func(m *Migration) {
-				src := m.Endpoint.SrcClient.(*fakeClient)
+			func(src, dst *fakeClient) {
 				src.errors.getProject = errors.New("err")
 			},
-			func(err error, m *Migration) {
+			func(err error, src, dst *fakeClient) {
 				assert.Error(err)
-				m.Endpoint.SrcClient.(*fakeClient).errors.getProject = nil
+				src.errors.getProject = nil
 			},
 		},
 		{
 			"copy 2 labels only",
 			cfg1,
-			func(m *Migration) {
-				src := m.Endpoint.SrcClient.(*fakeClient)
+			func(src, dst *fakeClient) {
 				src.labels = makeLabels("bug", "doc")
 			},
-			func(err error, m *Migration) {
+			func(err error, src, dst *fakeClient) {
 				require.NoError(err)
-				fk := m.Endpoint.DstClient.(*fakeClient)
-				if assert.Equal(2, len(fk.labels)) {
-					assert.Equal("bug", fk.labels[0].Name)
-					assert.Equal("doc", fk.labels[1].Name)
+				if assert.Equal(2, len(dst.labels)) {
+					assert.Equal("bug", dst.labels[0].Name)
+					assert.Equal("doc", dst.labels[1].Name)
 				}
 			},
 		},
 		{
 			"copy 1 label and 2 issues",
 			cfg2,
-			func(m *Migration) {
-				src := m.Endpoint.SrcClient.(*fakeClient)
+			func(src, dst *fakeClient) {
 				src.labels = makeLabels("P0")
 			},
-			func(err error, m *Migration) {
+			func(err error, src, dst *fakeClient) {
 				require.NoError(err)
-				fk := m.Endpoint.DstClient.(*fakeClient)
-				if assert.Equal(1, len(fk.labels)) {
-					assert.Equal("P0", fk.labels[0].Name)
+				if assert.Equal(1, len(dst.labels)) {
+					assert.Equal("P0", dst.labels[0].Name)
 				}
 			},
 		},
 		{
 			"copy milestones only",
 			cfg3,
-			func(m *Migration) {
-				src := m.Endpoint.SrcClient.(*fakeClient)
+			func(src, dst *fakeClient) {
 				src.clearMilestones()
 				src.milestones = makeMilestones("v1", "v2")
 			},
-			func(err error, m *Migration) {
+			func(err error, src, dst *fakeClient) {
 				require.NoError(err)
-				fk := m.Endpoint.DstClient.(*fakeClient)
-				if assert.Equal(2, len(fk.milestones)) {
-					assert.Equal("v1", fk.milestones[0].Title)
-					assert.Equal("v2", fk.milestones[1].Title)
+				if assert.Equal(2, len(dst.milestones)) {
+					assert.Equal("v1", dst.milestones[0].Title)
+					assert.Equal("v2", dst.milestones[1].Title)
 				}
 			},
 		},
 		{
 			"copy milestones only, error listing milestones",
 			cfg3,
-			func(m *Migration) {
-				src := m.Endpoint.SrcClient.(*fakeClient)
+			func(src, dst *fakeClient) {
 				src.clearMilestones()
 				src.milestones = makeMilestones("v1")
 				src.errors.listMilestones = errors.New("err")
 			},
-			func(err error, m *Migration) {
+			func(err error, src, dst *fakeClient) {
 				assert.Error(err)
-				m.Endpoint.SrcClient.(*fakeClient).errors.listMilestones = nil
+				src.errors.listMilestones = nil
 			},
 		},
 		{
 			"copy milestones only, error creating milestones",
 			cfg3,
-			func(m *Migration) {
-				src := m.Endpoint.SrcClient.(*fakeClient)
-				dst := m.Endpoint.DstClient.(*fakeClient)
+			func(src, dst *fakeClient) {
 				src.clearMilestones()
 				src.milestones = makeMilestones("v1")
 				dst.errors.createMilestone = errors.New("err")
 			},
-			func(err error, m *Migration) {
+			func(err error, src, dst *fakeClient) {
 				assert.Error(err)
-				m.Endpoint.DstClient.(*fakeClient).errors.createMilestone = nil
+				dst.errors.createMilestone = nil
 			},
 		},
 		{
 			"list labels fails",
 			cfg3,
-			func(m *Migration) {
-				src := m.Endpoint.SrcClient.(*fakeClient)
+			func(src, dst *fakeClient) {
 				src.errors.listLabels = errors.New("err")
 			},
-			func(err error, m *Migration) {
+			func(err error, src, dst *fakeClient) {
 				assert.Error(err)
-				src := m.Endpoint.SrcClient.(*fakeClient)
 				src.errors.listLabels = nil
 			},
 		},
 		{
 			"create labels fails",
 			cfg3,
-			func(m *Migration) {
-				src := m.Endpoint.SrcClient.(*fakeClient)
-				dst := m.Endpoint.DstClient.(*fakeClient)
+			func(src, dst *fakeClient) {
 				src.clearLabels()
 				src.labels = makeLabels("P0")
 				dst.errors.createLabel = errors.New("err")
 			},
-			func(err error, m *Migration) {
+			func(err error, src, dst *fakeClient) {
 				require.Error(err)
-				dst := m.Endpoint.DstClient.(*fakeClient)
 				dst.errors.createLabel = nil
 			},
 		},
 		{
 			"copy milestone only state closed",
 			cfg3,
-			func(m *Migration) {
-				src := m.Endpoint.SrcClient.(*fakeClient)
+			func(src, dst *fakeClient) {
 				src.clearMilestones()
 				src.milestones = makeMilestones("v1")
 				src.milestones[0].State = "closed"
 			},
-			func(err error, m *Migration) {
+			func(err error, src, dst *fakeClient) {
 				require.NoError(err)
-				dst := m.Endpoint.DstClient.(*fakeClient)
 				if assert.Equal(1, len(dst.milestones)) {
 					assert.Equal("close", dst.milestones[0].State)
 				}
@@ -169,17 +160,14 @@ func TestMigrate(t *testing.T) {
 		{
 			"copy closed milestone fails",
 			cfg3,
-			func(m *Migration) {
-				src := m.Endpoint.SrcClient.(*fakeClient)
-				dst := m.Endpoint.DstClient.(*fakeClient)
+			func(src, dst *fakeClient) {
 				src.clearMilestones()
 				src.milestones = makeMilestones("v1")
 				src.milestones[0].State = "closed"
 				dst.errors.updateMilestone = errors.New("err")
 			},
-			func(err error, m *Migration) {
+			func(err error, src, dst *fakeClient) {
 				require.Error(err)
-				dst := m.Endpoint.DstClient.(*fakeClient)
 				dst.errors.updateMilestone = nil
 			},
 		},
@@ -193,11 +181,11 @@ func TestMigrate(t *testing.T) {
 			m, err = New(conf)
 			require.NoError(err)
 			// Setup.
-			run.setup(m)
+			run.setup(source(m), dest(m))
 			// Run the migration.
 			err = m.Migrate()
 			// Asserts and tear down.
-			run.asserts(err, m)
+			run.asserts(err, source(m), dest(m))
 		})
 	}
 }
